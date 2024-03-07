8f78fe66f633a6bd6564e861e6a1f66c181b1b37
JaeMin_YU <jack7623@icloud.com>
2024-03-07 10:01:38 +0900

    First Commit

---
+++ constants/constants.js
@@ -0,0 +1,62 @@
+export const NAME = 'Jaemi Yu';
+
+export const DESIGNATION = `DEV PM | FullStack Developer`;
+
+export const DETAILS = {
+  Residence: 'Korea',
+  City: 'Daegu',
+  Age: '26',
+};
+
+export const SKILLS = [
+  {
+    title: 'DEV PM',
+    level: '70%',
+  },
+  {
+    title: 'FullStack Developer',
+    level: '30%',
+  },
+  {
+    title: 'iOS Developer',
+    level: '20%',
+  },
+  {
+    title: 'QA Manager',
+    level: '80%',
+  },
+];
+
+export const TECH_STACK = [
+  'python',
+  'C',
+  'JAVA',
+  'Swift',
+  'HTML',
+  'XML',
+  'CSS',
+  'JavaScript',
+  'NextJS',
+  'Git',
+  'Github',
+  'MS SQL',
+  'My SQL',
+  'Figma',
+  'VSC',
+  'Intelli J',
+  'Pycharm',
+  'Xcode',
+];
+
+export const CONTACTS = {
+  EMAIL: 'jack7623@icloud.com',
+  PHONE: '+82 010-2713-7653',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/JaeMin-YU',
+  LINKEDIN: 'https://www.linkedin.com/in/jaemin-yu-4a90632b9',
+  TWITTER: 'https://twitter.com/iosamajavaid',
+  INSTAGRAM: 'https://www.facebook.com/iosamajavaid',
+};
+//트위터랑 페북 >> 인스타랑 인크루트로 변경 하기
+//정보 업데이트 해야됨
