12c29665d9ededd57c9fca1c94691bcd94f4c3da
JaeMin_YU <jack7623@icloud.com>
2024-03-18 10:55:52 +0900

    Edit Profile image

--- components/Common/Intro/index.jsx
+++ components/Common/Intro/index.jsx
@@ -9,14 +9,13 @@ import Tools from './Tools';
 import Skills from './Skills';
 import Image from 'next/image';
 import { NAME, DESIGNATION, SOCIAL_LINKS } from '../../../constants/constants';
-import JaeminA from '../../../public/images/JaeminA.jpg';
 
 const Intro = () => {
   return (
     <>
       {/* fixed at top */}
       <div className='headerr z-50 absolute bg-MidNightBlack backdrop-blur-sm inset-y-0 h-48 top-0 flex items-center justify-center w-full flex-col px-4 gap-y-4'>
-        <Image className='w-20 h-50 rounded-full' src={JaeminA} alt='profile picture' />
+        <img className='w-20 h-50 rounded-full' src="images/JaeminA.jpg" alt="Jaemin" />
         <div className='flex flex-col items-center justify-center'>
           <span className='text-gray-300 text-base font-bold break-normal'>{NAME}</span>
           <span className='text-sm text-LightGray text-center mt-2'>{DESIGNATION}</span>
